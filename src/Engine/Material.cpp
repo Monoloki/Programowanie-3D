59d5ba6ed80a6d4685c75e8a2942052363750086
Grognor <Rugall1998@gmail.com>
2024-02-05 00:41:34 +0100

    robie co moge (textures)

--- src/Engine/Material.cpp
+++ src/Engine/Material.cpp
@@ -10,18 +10,34 @@ namespace xe {
 
     GLuint ColorMaterial::color_uniform_buffer_ = 0u;
     GLuint ColorMaterial::shader_ = 0u;
+    GLint  ColorMaterial::uniform_map_Kd_location_ = 0;
 
     void ColorMaterial::bind() {
+
+        int use_map_Kd = 0;
+        
+        if (texture_ > 0) {
+            OGL_CALL(glUniform1i(uniform_map_Kd_location_, texture_unit_));
+            OGL_CALL(glActiveTexture(GL_TEXTURE0 + texture_unit_));
+            OGL_CALL(glBindTexture(GL_TEXTURE_2D, texture_));
+            use_map_Kd = 1;
+        }
+
         glBindBufferBase(GL_UNIFORM_BUFFER, 0, color_uniform_buffer_);
         glUseProgram(program());
         glBindBuffer(GL_UNIFORM_BUFFER, color_uniform_buffer_);
         glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::vec4), &color_[0]);
+
+        glBufferSubData(GL_UNIFORM_BUFFER, 4 * sizeof(float), sizeof(GLint), &use_map_Kd);
+
         glBindBuffer(GL_UNIFORM_BUFFER, 0u);
 
     }
 
 
     void ColorMaterial::init() {
+
+        
 
 
         auto program = xe::utils::create_program(
@@ -33,6 +49,9 @@ namespace xe {
         }
 
         shader_ = program;
+
+        uniform_map_Kd_location_ = glGetUniformLocation(shader_, "map_Kd");
+
 
         glGenBuffers(1, &color_uniform_buffer_);
 
--- src/Engine/Material.h
+++ src/Engine/Material.h
@@ -28,12 +28,23 @@ namespace xe {
 
         static GLuint program() { return shader_; }
 
+        void set_texture(GLuint tex) { texture_ = tex; }
+
+        void set_texture_unit(GLuint tex_unit) { texture_unit_ = tex_unit; }
+
+        GLuint get_texture() { return texture_; };
+
+        GLuint get_texture_unit() { return texture_unit_; };
+
     private:
         static GLuint shader_;
         static GLuint color_uniform_buffer_;
-
+        
         glm::vec4 color_;
 
+        static GLint uniform_map_Kd_location_;
+        GLuint texture_ = 0;
+        GLuint texture_unit_ = 0;
     };
 
 
